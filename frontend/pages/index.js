0f48ba737dcbbe738256790385a54e0bd1bb89c2
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-05 19:32:04 +0530

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from Zeno 🚀</h1>
+      <p>Frontend deployed on Vercel successfully.</p>
+    </div>
+  );
+}
