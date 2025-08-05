4df2bdc57fc2f60102f378794847ca5733a1e0ff
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-05 17:45:53 +0530

    Update index.js

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,3 +1,3 @@
-export async function onRequest(context) {
-  return new Response("Hello from Zeno Backend!", { status: 200 });
+export function onRequest(context) {
+  return new Response("Hello, world!");
 }
