5b62d8bdf76bf8ab1e8073926078eb7ac8c16b19
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-05 17:41:52 +0530

    Update ping.js

--- backend/functions/ping.js
+++ backend/functions/ping.js
@@ -1,6 +1,9 @@
-export async function onRequest() {
+export async function onRequest(context) {
   return new Response(
     JSON.stringify({ status: "ok", message: "pong" }),
-    { status: 200, headers: { "Content-Type": "application/json" } }
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" }
+    }
   );
 }
