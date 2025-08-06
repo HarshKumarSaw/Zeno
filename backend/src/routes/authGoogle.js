525c01e9375173cdda678cd8533f8d56eaf0bef2
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-06 14:55:28 +0530

    Update authGoogle.js

--- backend/src/routes/authGoogle.js
+++ backend/src/routes/authGoogle.js
@@ -1,7 +1,7 @@
 // src/routes/authGoogle.js
 export async function onRequestGet(context) {
   const clientId = context.env.GOOGLE_CLIENT_ID;
-  const redirectUri = "https://zeno-frontend-ten.vercel.app/api/auth/google/callback";
+  const redirectUri = "https://zeno-backend.harshsaw01.workers.dev/api/auth/google/callback";
   const scope = "https://www.googleapis.com/auth/calendar.readonly";
 
   const authUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth");
