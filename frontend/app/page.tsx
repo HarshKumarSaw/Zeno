dcaefe9a5e85306e1cc0b4a9c6754e5260488a71
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-07 12:47:04 +0530

    Create page.tsx

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+// /app/page.tsx
+import Timeline from '../components/Timeline';
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-gray-50 dark:bg-black">
+      <Timeline />
+    </main>
+  );
+}
