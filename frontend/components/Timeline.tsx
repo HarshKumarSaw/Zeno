76cceabaef97f7371fbae78bb3c49ed9a3ea65af
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-07 13:05:11 +0530

    Update Timeline.tsx

--- frontend/components/Timeline.tsx
+++ frontend/components/Timeline.tsx
@@ -6,10 +6,17 @@ const hours = Array.from({ length: 24 }, (_, i) => `${i.toString().padStart(2, '
 export default function Timeline() {
   return (
     <div className="h-screen overflow-y-scroll bg-white dark:bg-black">
-      <div className="relative border-l border-gray-300 ml-14">
-        {hours.map((hour, idx) => (
-          <div key={idx} className="h-24 relative">
-            <div className="absolute -left-14 text-sm text-gray-500 dark:text-gray-400 w-12 text-right pr-2">
+      <div className="relative ml-14 border-l border-gray-300 dark:border-gray-700">
+        {hours.map((hour) => (
+          <div
+            key={hour}
+            className="h-24 relative"
+            role="presentation"
+          >
+            <div
+              className="absolute -left-14 w-12 pr-2 text-right text-sm text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+            >
               {hour}
             </div>
             <div className="absolute left-0 right-0 border-t border-dashed border-gray-200 dark:border-gray-700" />
