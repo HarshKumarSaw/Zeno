66bd673cc3e43c5f905a3aa923a21d755bb9493f
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-07 13:07:10 +0530

    Create colors.ts

---
+++ frontend/constants/colors.ts
@@ -0,0 +1,9 @@
+// /constants/colors.ts
+
+export const CALENDAR_COLORS = {
+  work: '#4285F4',        // Blue
+  personal: '#DB4437',    // Red
+  focus: '#F4B400',       // Yellow
+  wellness: '#0F9D58',    // Green
+  default: '#9E9E9E',     // Gray
+};
