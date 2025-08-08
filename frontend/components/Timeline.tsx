b4a35c1d061111439b88df82970665fc3e6a1fe1
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-08 07:11:15 +0530

    Update Timeline.tsx

--- frontend/components/Timeline.tsx
+++ frontend/components/Timeline.tsx
@@ -31,9 +31,6 @@ export default function Timeline() {
             <div className="absolute left-0 right-0 border-t border-dashed border-gray-200 dark:border-gray-700" />
           </div>
         ))}
-
-        {/* Events overlay */}
-        <TimelineEvents />
       </div>
     </div>
   );
