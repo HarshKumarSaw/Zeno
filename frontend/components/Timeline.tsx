28d3425910b58de480427752a814f093e4371355
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-08 08:06:47 +0530

    Update Timeline.tsx

--- frontend/components/Timeline.tsx
+++ frontend/components/Timeline.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import TimelineEventComponent from './TimelineEvent';
+import TimelineEventComponent from './TimelineEvents';
 import { TimelineEvent } from '../types/event';
 
 export default function Timeline() {
