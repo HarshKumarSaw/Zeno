9dd4fa6e209fb9b7466b0994fe5b318dacd4bc7e
Harsh Kumar Saw <harshsaw01@gmail.com>
2025-08-08 10:02:49 +0530

    Create cors.js

---
+++ backend/src/utils/cors.js
@@ -0,0 +1,8 @@
+const allowedOrigin = process.env.ALLOWED_ORIGIN || "*"; // "*" for dev, env var for prod
+
+export function withCorsHeaders(resp) {
+  resp.headers.set("Access-Control-Allow-Origin", allowedOrigin);
+  resp.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+  resp.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  return resp;
+}
